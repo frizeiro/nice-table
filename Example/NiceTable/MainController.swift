837d20751ca5b31f56f973606941fab083e274c4
Felipe Frizeiro <felipefrizeiro@gmail.com>
2025-02-23 17:39:29 -0300

    Updating example

--- Example/NiceTable/MainController.swift
+++ Example/NiceTable/MainController.swift
@@ -11,26 +11,27 @@ import NiceTable
 
 class MainController: NiceTableViewController {
     
+    // MARK: - Private Variables
+    
     private lazy var sections: [NiceTableSection] = {
         [
             NiceTableSection(
-                animals,
-                title: "Animals"
+                contentCellIcon,
+                title: "Content Item with icon"
             ),
             NiceTableSection(
-                transportation,
-                title: "Transportation"
+                contentCell,
+                title: "Content Item without icon"
+            ),
+            NiceTableSection(
+                switchItems,
+                title: "Switch Item"
             )
         ]
     }()
     
-    private lazy var animals: [NiceTableItem] = {
+    private lazy var contentCellIcon: [NiceTableItem] = {
         [
-            NiceContentItem(.description(
-                label: "Birds",
-                text: "Birds are a group of warm-blooded vertebrates constituting the class Aves.",
-                image: image("bird.fill")
-            )),
             NiceContentItem(.description(
                 label: "Dogs",
                 text: "A dog is a domestic mammal of the family Canidae and the order Carnivora.",
@@ -44,25 +45,35 @@ class MainController: NiceTableViewController {
         ]
     }()
     
-    private lazy var transportation: [NiceTableItem] = {
+    private lazy var contentCell: [NiceTableItem] = {
         [
             NiceContentItem(.description(
-                label: "Car",
-                text: "A vehicle that has wheels, carries a small number of passengers, and is moved by an engine or a motor.",
-                image: image("car")
-            )),
-            NiceContentItem(.description(
-                label: "Bus",
-                text: "A large motor vehicle, having a long body, equipped with seats or benches for passengers, usually operating as part of a scheduled service.",
-                image: image("bus")
-            )),
-            NiceContentItem(.description(
-                label: "Cablecar",
-                text: "Cable railway, in which the vehicle rests on rails or a road. Cable car (railway), a type of cable transportation used for mass transit.",
-                image: image("cablecar")
+                label: "Aves",
+                subtitle: "Warm-blooded vertebrates",
+                text: "Birds are a group of warm-blooded vertebrates constituting the class Aves. Which is further divided into orders.",
+                title: "Birds",
+                footer: "Orders are groups of birds with similar characteristics"
             ))
         ]
     }()
+    
+    private lazy var switchItems: [NiceTableItem] = {
+        [
+            NiceSwitchItem(.description(
+                label: "Car",
+                text: "Do you have a car?",
+                image: image("car"),
+                footer: "A vehicle that has wheels, carries a small number of passengers, and is moved by an engine or a motor."
+            )),
+            NiceSwitchItem(.description(
+                label: "Bus",
+                text: "Do you go work by bus?",
+                image: image("bus")
+            ))
+        ]
+    }()
+    
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,6 +83,8 @@ class MainController: NiceTableViewController {
         
         tableView?.sections = sections
     }
+    
+    // MARK: - Private Methods
     
     private func image(_ named: String) -> UIImage? {
         if #available(iOS 13.0, *) {
